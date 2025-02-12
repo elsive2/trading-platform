678661f6298c587f2e5d8e61fafcc90f4648cac1
maxim <maximyakovlev228@gmail.com>
2025-02-13 00:12:18 +0500

    feat: Set up discovery service

---
+++ service-discovery/src/test/java/com/trading_platform/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading_platform.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
