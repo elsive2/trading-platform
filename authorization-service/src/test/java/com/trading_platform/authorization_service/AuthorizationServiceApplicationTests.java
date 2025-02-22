c123c5f23db34766e52b64b1f445b363d6cced47
maxim <maximyakovlev228@gmail.com>
2025-02-22 21:56:44 +0500

    feat: Init authentication service

---
+++ authorization-service/src/test/java/com/trading_platform/authorization_service/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading_platform.authorization_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
