695381e5d0b3f5f248db38bd400c6456cf585bad
maxim <maximyakovlev228@gmail.com>
2025-04-10 00:49:38 +0500

    fix[transaction-service]: Delete service

--- transaction-service/src/main/resources/db/migration/V1__init_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE transactions (
-    id UUID PRIMARY KEY,
-    user_id VARCHAR(50) NOT NULL,
-    stock_id INT NOT NULL,
-    action VARCHAR(20) NOT NULL, -- BUY / SELL
-    quantity INT NOT NULL,
-    total_cost NUMERIC(15, 2) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT now()
-);
